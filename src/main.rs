43f20c1439bdff1dd712c9d6293f326bd356a1f6
Erin Power <xampprocky@gmail.com>
2021-06-21 15:46:37 +0200

    Switch to actix from rocket

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,103 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+use std::process::Command;
 
-#[macro_use]
-extern crate rocket;
-
-use std::{io::Cursor, process::Command};
-
-use badge::{Badge, BadgeOptions};
-use once_cell::sync::Lazy;
-use rocket::{
-    http::{hyper::header::EntityTag, Accept, ContentType, Status},
-    response::Redirect,
-    Response,
+use actix_http::http::header::{
+    Accept, CacheControl, CacheDirective, ContentType, EntityTag, Header, IfNoneMatch,
+    CACHE_CONTROL, CONTENT_TYPE, ETAG, LOCATION,
 };
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer};
+use badge::{Badge, BadgeOptions};
+use cached::Cached;
+use once_cell::sync::Lazy;
 use tempfile::TempDir;
 use tokei::{Language, Languages};
-
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 const BILLION: usize = 1_000_000_000;
 const BLANKS: &str = "blank lines";
 const BLUE: &str = "#007ec6";
-const GREY: &str = "#9e9e9e";
 const CODE: &str = "lines of code";
 const COMMENTS: &str = "comments";
 const FILES: &str = "files";
 const HASH_LENGTH: usize = 40;
 const LINES: &str = "total lines";
 const MILLION: usize = 1_000_000;
-const RED: &str = "#e05d44";
 const THOUSAND: usize = 1_000;
-const DAY_IN_SECONDS: usize = 24 * 60 * 60;
+const DAY_IN_SECONDS: u64 = 24 * 60 * 60;
 
-static BAD_URL_BADGE: Lazy<String> = Lazy::new(|| {
-    let options = BadgeOptions {
-        subject: String::from("Error"),
-        status: String::from("Incorrect URL"),
-        color: String::from(RED),
-    };
+static CONTENT_TYPE_SVG: Lazy<ContentType> =
+    Lazy::new(|| ContentType("image/svg".parse().unwrap()));
 
-    Badge::new(options).unwrap().to_svg()
-});
-
-static INVALID_SHA_BADGE: Lazy<String> = Lazy::new(|| {
-    let options = BadgeOptions {
-        subject: String::from("Error"),
-        status: String::from("Invalid commit SHA"),
-        color: String::from(RED),
-    };
-
-    Badge::new(options).unwrap().to_svg()
-});
-
-static PROCESSING_BADGE: Lazy<String> = Lazy::new(|| {
-    let options = BadgeOptions {
-        subject: String::from("Error"),
-        status: String::from("Procsssing…"),
-        color: String::from(RED),
-    };
-
-    Badge::new(options).unwrap().to_svg()
-});
-
-fn main() {
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::init();
     dotenv::dotenv().unwrap();
-    rocket::ignite().mount("/", routes![index, badge]).launch();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(actix_web::middleware::Logger::default())
+            .service(redirect_index)
+            .service(create_badge)
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
 }
 
 #[get("/")]
-fn index() -> Redirect {
-    Redirect::permanent("https://github.com/XAMPPRocky/tokei")
-}
-
-struct IfNoneMatch(Option<EntityTag>);
-
-impl<'a, 'r> rocket::request::FromRequest<'a, 'r> for IfNoneMatch {
-    type Error = ();
-
-    fn from_request(
-        request: &'a rocket::Request<'r>,
-    ) -> rocket::request::Outcome<Self, Self::Error> {
-        rocket::Outcome::Success(Self(
-            request
-                .headers()
-                .get("If-None-Match")
-                .next()
-                .and_then(|s| s.parse().ok()),
-        ))
-    }
+fn redirect_index() -> HttpResponse {
+    HttpResponse::PermanentRedirect()
+        .header(LOCATION, "https://github.com/XAMPPRocky/tokei")
+        .finish()
 }
 
 macro_rules! respond {
-    ($status:expr) => {{
-        let mut response = Response::new();
-        response.set_status($status);
-        Ok(response)
+    ($status:ident) => {{
+        HttpResponse::$status().finish()
     }};
 
-    ($status:expr, $body:expr) => {{
-        let mut response = Response::new();
-        response.set_status($status);
-        response.set_sized_body(Cursor::new($body));
-        response.set_header(ContentType::SVG);
-        Ok(response)
+    ($status:ident, $body:expr) => {{
+        HttpResponse::$status()
+            .set(CONTENT_TYPE_SVG.clone())
+            .body($body)
     }};
 
-    ($status:expr, $accept:expr, $body:expr, $etag:expr) => {{
-        use rocket::http::hyper::header::{CacheControl, CacheDirective, ETag};
-
-        let mut response = Response::new();
-        response.set_status($status);
-        response.set_sized_body(Cursor::new($body));
-        response.set_header(if *$accept == Accept::JSON {
-            ContentType::JSON
-        } else {
-            ContentType::SVG
-        });
-        response.set_header(CacheControl(vec![CacheDirective::NoCache]));
-        response.set_header(ETag(EntityTag::new(false, $etag)));
-        Ok(response)
+    ($status:ident, $accept:expr, $body:expr, $etag:expr) => {{
+        HttpResponse::$status()
+            .header(CACHE_CONTROL, CacheControl(vec![CacheDirective::NoCache]))
+            .header(ETAG, EntityTag::new(false, $etag))
+            .header(
+                CONTENT_TYPE,
+                if $accept == ContentType::json() {
+                    ContentType::json()
+                } else {
+                    CONTENT_TYPE_SVG.clone()
+                },
+            )
+            .body($body)
     }};
 }
 
-
-#[get("/b1/<domain>/<user>/<repo>?<category>")]
-fn badge<'response>(
-    accept_header: &Accept,
-    if_none_match: IfNoneMatch,
-    domain: String,
-    user: String,
-    repo: String,
+#[derive(serde::Deserialize)]
+struct BadgeQuery {
     category: Option<String>,
-) -> Result<Response<'response>> {
-    let category = category.unwrap_or(String::from("lines"));
+}
+
+#[get("/b1/{domain}/{user}/{repo}")]
+async fn create_badge(
+    request: HttpRequest,
+    web::Path((domain, user, repo)): web::Path<(String, String, String)>,
+    web::Query(query): web::Query<BadgeQuery>,
+) -> actix_web::Result<HttpResponse> {
+    let category = query.category.unwrap_or(String::from("lines"));
+
+    let content_type = if let Ok(accept) = Accept::parse(&request) {
+        if accept == Accept::json() {
+            ContentType::json()
+        } else {
+            CONTENT_TYPE_SVG.clone()
+        }
+    } else {
+        CONTENT_TYPE_SVG.clone()
+    };
 
     let mut domain = percent_encoding::percent_decode_str(&domain).decode_utf8()?;
 
@@ -142,60 +108,68 @@ fn badge<'response>(
 
     let url = format!("https://{}/{}/{}", domain, user, repo);
     let ls_remote = Command::new("git").arg("ls-remote").arg(&url).output()?;
-    let stdout = ls_remote.stdout;
-    let end_of_sha = match stdout.iter().position(|&b| b == b'\t') {
-        Some(index) if index == HASH_LENGTH => index,
-        _ => return respond!(Status::BadRequest, &**BAD_URL_BADGE),
-    };
-    let sha = match String::from_utf8(stdout[..end_of_sha].to_owned()) {
-        Ok(s) => s,
-        Err(_) => return respond!(Status::BadRequest, &**INVALID_SHA_BADGE),
-    };
+    let sha: String = ls_remote
+        .stdout
+        .iter()
+        .position(|&b| b == b'\t')
+        .filter(|i| *i == HASH_LENGTH)
+        .map(|i| (&ls_remote.stdout[..i]).to_owned())
+        .and_then(|bytes| String::from_utf8(bytes).ok())
+        .ok_or_else(|| actix_web::error::ErrorBadRequest(eyre::eyre!("Invalid SHA provided.")))?;
 
-    let entry = get_statistics(&url, &sha)?;
+    if let Ok(if_none_match) = IfNoneMatch::parse(&request) {
+        let sha_tag = EntityTag::new(false, sha.clone());
+        let found_match = match if_none_match {
+            IfNoneMatch::Any => false,
+            IfNoneMatch::Items(items) => items.iter().any(|etag| etag.weak_eq(&sha_tag)),
+        };
+
+        if found_match {
+            CACHE
+                .lock()
+                .unwrap()
+                .cache_get(&repo_identifier(&url, &sha));
+            log::info!("{}#{} Not Modified", url, sha);
+            return Ok(respond!(NotModified));
+        }
+    }
+
+    let entry = get_statistics(&url, &sha).map_err(|err| actix_web::error::ErrorBadRequest(err))?;
 
     if entry.was_cached {
         log::info!("{}#{} Cache hit", url, sha);
-
-        if let IfNoneMatch(Some(etag)) = if_none_match {
-            let sha_tag = EntityTag::new(false, sha.clone());
-            if sha_tag.weak_eq(&etag) {
-                log::info!("{}#{} Not Modified", url, sha);
-                return respond!(Status::NotModified);
-            }
-        }
     }
 
     let stats = entry.value;
 
     log::info!(
         "{url}#{sha} - Lines {lines} Code {code} Comments {comments} Blanks {blanks}",
-        url=url,
-        sha=sha,
-        lines=stats.lines,
-        code=stats.code,
-        comments=stats.comments,
-        blanks=stats.blanks
+        url = url,
+        sha = sha,
+        lines = stats.lines,
+        code = stats.code,
+        comments = stats.comments,
+        blanks = stats.blanks
     );
 
-    let badge = make_badge(accept_header, &stats, &category)?;
+    let badge = make_badge(&content_type, &stats, &category)?;
 
-    respond!(
-        Status::Ok,
-        accept_header,
-        badge,
-        sha
-    )
+    Ok(respond!(Ok, content_type, badge, sha))
+}
+
+fn repo_identifier(url: &str, sha: &str) -> String {
+    format!("{}#{}", url, sha)
 }
 
 #[cached::proc_macro::cached(
+    name = "CACHE",
     result = true,
     with_cached_flag = true,
     type = "cached::TimedSizedCache<String, cached::Return<Language>>",
-    create = "{ cached::TimedSizedCache::with_size_and_lifespan(1000, 86400) }",
-    convert = r#"{ format!("{}{}", url, _sha) }"#,
+    create = "{ cached::TimedSizedCache::with_size_and_lifespan(1000, DAY_IN_SECONDS) }",
+    convert = r#"{ repo_identifier(url, _sha) }"#
 )]
-fn get_statistics(url: &str, _sha: &str) -> Result<cached::Return<Language>> {
+fn get_statistics(url: &str, _sha: &str) -> eyre::Result<cached::Return<Language>> {
     log::info!("{} - Cloning", url);
     let temp_dir = TempDir::new()?;
     let temp_path = temp_dir.path().to_str().unwrap();
@@ -224,8 +198,12 @@ fn trim_and_float(num: usize, trim: usize) -> f64 {
     (num as f64) / (trim as f64)
 }
 
-fn make_badge(accept: &Accept, stats: &Language, category: &str) -> Result<String> {
-    if *accept == Accept::JSON {
+fn make_badge(
+    content_type: &ContentType,
+    stats: &Language,
+    category: &str,
+) -> actix_web::Result<String> {
+    if *content_type == ContentType::json() {
         return Ok(serde_json::to_string(&stats)?);
     }
 
